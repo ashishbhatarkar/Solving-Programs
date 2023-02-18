5d5f48b271256b6b4a9e9892364c569766b2ec01
ashishbhatarkar <ashu.bhatarkar@gmail.com>
2023-02-18 14:56:59 +0530

    Adding Switch case

--- src/SolvPrograms/Arithmetic_Calculator.java
+++ src/SolvPrograms/Arithmetic_Calculator.java
@@ -1,8 +1,35 @@
 package SolvPrograms;
 
+import java.util.Scanner;
+
 public class Arithmetic_Calculator {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Two Numbers");
+		int fc1 = sc.nextInt();
+		int fc2 = sc.nextInt();
+		
+		System.out.println("Enter Any one Calculation Method( / , * , + , - )");
+		
+		String cls = sc.nextLine();
+		int result = 0;
+		
+		switch(cls) {
+		
+		case "/" :
+			result = fc1/fc2;
+			System.out.println(fc1 + "/" + fc2 + "=" + result );
+			
+		case "*" :
+			result = fc1*fc2;
+			System.out.println(fc1 + "*" + fc2 + "=" + result );
+		
+		
+		
+		
+		}
 		
 
 	}
