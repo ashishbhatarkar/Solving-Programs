3c41d492aa610e1b136b94078f76c88bcb9538a6
ashishbhatarkar <ashu.bhatarkar@gmail.com>
2023-02-20 14:22:01 +0530

    Email Validating program

---
+++ src/SolvPrograms/EmailValidating.java
@@ -0,0 +1,24 @@
+package SolvPrograms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmailValidating {
+	
+	public static void main(String[] args) {
+		
+		List<String> lststr = new ArrayList<String>();
+		
+		lststr.add("ashish@email.com");
+		lststr.add("simplilearn@email.com");
+		lststr.add("venugopal@email.com");
+		lststr.add("saurabh@email.com");
+		lststr.add("bhatarkar@email.com");
+		
+		System.out.println(lststr);
+		
+	}
+	
+	
+
+}
